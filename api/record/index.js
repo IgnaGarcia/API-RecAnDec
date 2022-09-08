b8daec2d4407b99f6df69d653ff46790084339d2
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-08 19:27:57 -0300

    Add: GET records with query

--- api/record/index.js
+++ api/record/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const recordController = require('./record.controller');
 
 router.post('/:id/record', recordController.post);
+router.get('/:id/record', recordController.get);
 
 module.exports = router;
--- api/record/record.controller.js
+++ api/record/record.controller.js
@@ -30,4 +30,34 @@ const post = async(req, res) => {
     } else return res.status(400).json({ message: "Fields required are null" })
 }
 
-module.exports = { post }
+const get = async(req, res) => {
+    console.log("[GET]: records, Body: " + JSON.stringify(req.body))
+   
+    let query = { 'owner': req.params.id}
+    if(req.body.isOut != undefined) query['isOut'] = req.body.isOut
+    if(req.body.dateFrom && req.body.dateUntil) query['date'] =  {
+        $gte: new Date(req.body.dateFrom), 
+        $lt: new Date(req.body.dateUntil)
+    }
+    if(req.body.categories && req.body.categories.length > 0) query['category'] = { $in: req.body.categories }
+    if(req.body.tags && req.body.tags.length > 0) query['tags'] = { $in: req.body.tags }
+    if(req.body.wallets && req.body.wallets.length > 0) query['wallets'] = { $in: req.body.wallets }
+    console.log(`[QUERY]: ${JSON.stringify(query)}`)
+
+    try {
+        const records = await Record.find(query).exec()
+        console.log(`[RECORDS FINDED]: ${records.length}`)
+        res.status(200).json({
+            message: "Record finded successfully",
+            data: records
+        });
+    } catch (err) {
+        console.error("[ERROR]" + err)
+        res.status(500).json({
+            message: "Internal Server Error on Finding",
+            error: err
+        });
+    }
+}
+
+module.exports = { post, get }
