4cfe0e460417a8bdffba4047199b0bb44c9021bf
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-14 19:25:07 -0300

    Add: init command routes

---
+++ api/command/command.controller.js
@@ -0,0 +1,66 @@
+const Command = require('./command.model');
+
+
+const post = async(req, res) => {
+    console.log("[POST]: command ")
+    if(req.params.id && req.body) {
+    /*
+    Path: userId
+    Request:
+        expense: bool
+    category: string
+    tags: [string]
+    wallet: string
+    Response:
+        message: string
+        code: int
+        data: string
+
+    */
+    } else return res.status(400).json({ message: "Fields  required are null"})
+}
+
+const get = async(req, res) => {
+    console.log("[GET]: command")
+    /*
+    Path: userId
+    Request:
+    Response:
+        message: string
+        code: int
+        data: 
+            expenseCommand: Command
+            incomeCommand: Command
+    */
+}
+
+const update = async(req, res) => {
+    console.log("[PUT]: command")
+    /*
+    Path: userId
+    Request:
+        expense: bool
+    category: string
+    tags: [string]
+    wallet: string
+    Response:
+        message: string
+        code: int
+    	data: string
+    */
+}
+
+const erase = async(req, res) => {
+    console.log("[DELETE]: command")
+    /*
+    Path: userId
+    Request:
+        expense: bool
+    Response:
+        message: string
+        code: int
+        data: string
+    */
+}
+
+module.exports = { post, get, update, erase }
--- api/routes.js
+++ api/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const recordRoutes = require('./record/index');
 const categoryRoutes = require('./category/index');
 const limitRoutes = require('./limit/index');
+const commandRoutes = require('./command/index');
 
 const app = express();
 
 app.use('/user', recordRoutes);
 app.use('/user', categoryRoutes);
 app.use('/user', limitRoutes);
+app.use('/user', commandRoutes);
 
 module.exports = app;
