8c2afa0d8d49786788e18d9355d14b9584c13e66
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-14 20:03:35 -0300

    Add: get tags

--- api/tag/tag.controller.js
+++ api/tag/tag.controller.js
@@ -31,7 +31,26 @@ const post = async(req, res) => {
 
 const get = async(req, res) => {
     console.log("[GET]: tags")
-    
+    const page = req.query.page ? Number(req.query.page) : 1
+
+    let query = { 'owner': req.params.id }
+    console.log(`[QUERY]: ${JSON.stringify(query)}`)
+
+    try {
+        const paginatedResponse = await getWithPaging(Tag, query, { year: -1, month: -1 }, page)
+
+        console.log(`[TAGS FINDED]: ${paginatedResponse.data.length}`)
+        res.status(200).json({
+            message: "Tags finded successfully",
+            ...paginatedResponse
+        });
+    } catch (err) {
+        console.error("[ERROR]: " + err)
+        res.status(500).json({
+            message: "Internal Server Error on Finding",
+            error: err
+        });
+    }
 }
 
 module.exports = { post, get }
