b3985816ad56fcf25e0ce263f10602b775e869e1
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-18 18:48:30 -0300

    Refactor: category routes with verify token

--- api/category/index.js
+++ api/category/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const categoryController = require('./category.controller');
 
-router.post('/:id/category', categoryController.post);
-router.get('/:id/category', categoryController.get);
+router.post('/', verify, categoryController.post);
+router.get('/', verify, categoryController.get);
 
 module.exports = router;
