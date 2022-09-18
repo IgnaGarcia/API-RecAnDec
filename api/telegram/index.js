a6c20ff8ed683a954a753d177f4aa94d8d15186a
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-18 18:57:12 -0300

    Add: telegram get commands

--- api/telegram/index.js
+++ api/telegram/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const telegramController = require('./telegram.controller');
 
 router.post('/:telegramId', telegramController.sync);
+router.get('/:telegramId/commands', telegramController.commands);
 router.get('/:telegramId/wallets', telegramController.wallets);
 router.get('/:telegramId/categories', telegramController.categories);
 router.get('/:telegramId/tags', telegramController.tags);
