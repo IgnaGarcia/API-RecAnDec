19c0f23a4102f5c05648134c5a2983a3aa5a4e2d
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-18 18:54:14 -0300

    Refactor: Wallet routes with verify token

--- api/wallet/index.js
+++ api/wallet/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const walletController = require('./wallet.controller');
 
-router.post('/:id/wallet', walletController.post);
-router.get('/:id/wallet', walletController.get);
+router.post('/', verify, walletController.post);
+router.get('/', verify, walletController.get);
 
 module.exports = router;
