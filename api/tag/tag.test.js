30315dd3a72d14a865f3c3c3ac9f3c1336654ec3
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-25 12:41:25 -0300

    added: tag tests

---
+++ api/tag/tag.test.js
@@ -0,0 +1,72 @@
+const mockingoose = require('mockingoose')
+const httpMocks = require('node-mocks-http');
+const Tag = require('../tag/tag.model')
+const { tagsMock } = require('../../mocks/mocks')
+const { post, get } = require('./tag.controller');
+const { query } = require('express');
+
+describe('POST /tags', () => {
+    it('Should create new tag', async () => {
+        mockingoose(Tag).toReturn(tagsMock, "save")
+        const req = httpMocks.createRequest({
+            id: tagsMock[0].owner,
+            body: {
+                label: tagsMock[0].label,
+                alias: tagsMock[0].alias
+            }
+        })
+        const res = httpMocks.createResponse()
+
+        await post(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(201)
+        expect(json.data.label).toEqual(tagsMock[0].label)
+        expect(json.data.alias).toEqual(tagsMock[0].alias)
+    })
+    it('Should fail on empty label', async () => {
+        const req = httpMocks.createRequest({
+            id: tagsMock[0].owner,
+            body: {
+                alias: tagsMock[0].alias
+            }
+        })
+        const res = httpMocks.createResponse()
+
+        await post(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(400)
+        expect(json.message).toEqual("Fields required are null")
+    })
+})
+
+describe('GET /tags', () => {
+    it('Should find tags', async () => {
+        mockingoose(Tag).toReturn(tagsMock, "find")
+        const req = httpMocks.createRequest({
+            id: tagsMock[0].owner,
+            query: { page: 1 }
+        })
+        const res = httpMocks.createResponse()
+
+        await get(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(200)
+        expect(json.data.length).toEqual(3)
+    })
+    it('Should find tags whitout page', async () => {
+        mockingoose(Tag).toReturn(tagsMock, "find")
+        const req = httpMocks.createRequest({
+            id: tagsMock[0].owner
+        })
+        const res = httpMocks.createResponse()
+
+        await get(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(200)
+        expect(json.data.length).toEqual(3)
+    })
+})
