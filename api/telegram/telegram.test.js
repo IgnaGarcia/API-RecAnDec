b1e8a30435f13910ad44c62ac4558cf9495ec483
Ignacio Garcia <gnachoxp@gmail.com>
2022-09-24 22:06:03 -0300

    added: telegram tests

---
+++ api/telegram/telegram.test.js
@@ -0,0 +1,122 @@
+const mockingoose = require('mockingoose')
+const httpMocks = require('node-mocks-http');
+const User = require('../user/user.model')
+const Command = require('../command/command.model')
+const Wallet = require('../wallet/wallet.model')
+const Category = require('../category/category.model')
+const Tag = require('../tag/tag.model')
+const { userMock, walletsMock, commandsMock, categoriesMock, tagsMock } = require('../../mocks/mocks')
+const { sync, commands, wallets, categories, tags } = require('./telegram.controller')
+
+describe('POST /:telegramId', () => {
+    it('Should Validate TelegramId an set it', async () => {
+        mockingoose(User).toReturn(userMock, "findOne")
+        mockingoose(User).toReturn(userMock, "save")
+        const req = httpMocks.createRequest({
+            params: { telegramId: "44778822" },
+            body: {
+                email: userMock.email,
+                securityCode: userMock.telegramId
+            }
+        })
+        const res = httpMocks.createResponse()
+
+        await sync(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(200)
+        expect(json.data.telegramId).toEqual("44778822")
+        expect(json.data.password).not.toEqual("123")
+    })
+    it('Should not found user', async () => {
+        mockingoose(User).toReturn(null, "findOne")
+        const req = httpMocks.createRequest({
+            params: { telegramId: "44778822" },
+            body: {
+                email: userMock.email,
+                securityCode: userMock.telegramId
+            }
+        })
+        const res = httpMocks.createResponse()
+
+        await sync(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(404)
+        expect(json.message).toEqual("Invalid email or security code")
+    })
+    it('Should fail for empty data', async () => {
+        mockingoose(User).toReturn(userMock, "findOne")
+        const req = httpMocks.createRequest({
+            params: { telegramId: "44778822" },
+            body: {
+                securityCode: userMock.telegramId
+            }
+        })
+        const res = httpMocks.createResponse()
+
+        await sync(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(400)
+        expect(json.message).toEqual("Fields required are null")
+    })
+})
+
+describe('GET /:telegramId/commands', () => {
+    it('Should find commands', async () => {
+        mockingoose(Command).toReturn(commandsMock, "find")
+        const req = httpMocks.createRequest({
+            params: { telegramId: "44778822" }
+        })
+        const res = httpMocks.createResponse()
+
+        await commands(req, res)
+
+        const json = res._getJSONData()
+        expect(res.statusCode).toEqual(200)
+        expect(json.data.length).toEqual(2)
+    })
+})
+
+describe('GET /:telegramId/wallets', () => {
+    it('Should find wallets', async () => {
+        mockingoose(User).toReturn(userMock, "find")
+        mockingoose(Wallet).toReturn(walletsMock, "find")
+        const req = httpMocks.createRequest({
+            params: { telegramId: "44778822" }
+        })
+        const res = httpMocks.createResponse()
+
+        await wallets(req, res)
+        expect(res.statusCode).toEqual(200)
+    })
+})
+
+describe('GET /:telegramId/categories', () => {
+    it('Should find categories', async () => {
+        mockingoose(User).toReturn(userMock, "find")
+        mockingoose(Category).toReturn(categoriesMock, "find")
+        const req = httpMocks.createRequest({
+            params: { telegramId: "44778822" }
+        })
+        const res = httpMocks.createResponse()
+
+        await categories(req, res)
+        expect(res.statusCode).toEqual(200)
+    })
+})
+
+describe('GET /:telegramId/tags', () => {
+    it('Should find tags', async () => {
+        mockingoose(User).toReturn(userMock, "find")
+        mockingoose(Tag).toReturn(tagsMock, "find")
+        const req = httpMocks.createRequest({
+            params: { telegramId: "44778822" }
+        })
+        const res = httpMocks.createResponse()
+
+        await tags(req, res)
+        expect(res.statusCode).toEqual(200)
+    })
+})
--- mocks/mocks.js
+++ mocks/mocks.js
@@ -1,4 +1,4 @@
-user = {
+userMock = {
     "_id": "63190a6acf10c3930a8386be",
     "email": "gnachoxp@gmail.com",
     "password": "$2b$12$zYXkE639r0L.JlYVqYM1pODUXeknllVEB0YqZIhhX8HINF8yKpHXS",
@@ -6,7 +6,7 @@ user = {
     "telegramId": "982840555"
 }
 
-wallets = [
+walletsMock = [
     {
         "_id": "632262795bb58efd145caee3",
         "owner": "63190a6acf10c3930a8386be",
@@ -36,4 +36,71 @@ wallets = [
     }
 ]
 
-module.exports = { user, wallets }
+commandsMock = [
+    {
+        "_id": "632631295e6456f33c6c3937",
+        "owner": "63190a6acf10c3930a8386be",
+        "category": "63190ab3cf10c3930a8386bf",
+        "tags": null,
+        "wallet": null,
+        "expense": true,
+    },
+    {
+        "_id": "632631295e6456f33c6c3937",
+        "owner": "63190a6acf10c3930a8386be",
+        "category": "63190ab3cf10c3930a8386bf",
+        "tags": null,
+        "wallet": null,
+        "expense": false,
+    }
+]
+
+categoriesMock = [
+    {
+        "_id": "63190ab3cf10c3930a8386bf",
+        "owner":null,
+        "alias":"alim",
+        "label":"Alimentos",
+        "isOut":true
+    },
+    {
+        "_id": "63290ab3cf10c3930a8386bf",
+        "owner":null,
+        "alias":"imp",
+        "label":"Impuestos",
+        "isOut":true
+    },
+    {
+        "_id": "63390ab3cf10c3930a8386bf",
+        "owner":"63190a6acf10c3930a8386be",
+        "alias":"subs",
+        "label":"Subscripciones",
+        "isOut":true
+    }
+]
+
+tagsMock = [
+    {
+        "_id": "63225edaa4d8591844853a24",
+        "owner": "63190a6acf10c3930a8386be",
+        "label": "Recurrente",
+        "alias": "recu",
+        "createDate": "2022-09-14T23:23:37.596"
+    },
+    {
+        "_id": "63425edaa4d8591844853a24",
+        "owner": "63190a6acf10c3930a8386be",
+        "label": "Extraordinario",
+        "alias": "extra",
+        "createDate": "2022-09-14T23:23:37.596"
+    },
+    {
+        "_id": "63325edaa4d8591844853a24",
+        "owner": "63190a6acf10c3930a8386be",
+        "label": "Chucheria",
+        "alias": "chuch",
+        "createDate": "2022-09-14T23:23:37.596"
+    }
+]
+
+module.exports = { userMock, walletsMock, commandsMock, categoriesMock, tagsMock }
